9a60970595811604a89a34c4cee57a83e6079990
omocomo <omocomo@naver.com>
2024-05-28 11:09:24 +0900

    update db schemas and models

--- app/schemas.py
+++ app/schemas.py
@@ -1,17 +1,21 @@
 from pydantic import BaseModel
 from typing import List, Optional
+from datetime import datetime
 
 class ReviewBase(BaseModel):
-    star: int
-    long_term_use: bool
-    repurchase: bool
-    skin_type: str
-    skin_concern: str
-    irritation_level: str
-    cleansing_power: str
-    absorption_power: str
-    texture_power: str
+    product_name: str
+    reviewer_name: str
+    rating: int
+    used_over_one_month: bool
+    repurchase_intention: bool
+    skin_type_review: str
+    skin_concern_review: str
+    irritation_level_review: str
+    cleansing_power_review: Optional[str]
+    spreadability_review: Optional[str]
     review_content: str
+    review_date: Optional[datetime]
+
 
 class ReviewCreate(ReviewBase):
     product_id: int
@@ -25,31 +29,32 @@ class Review(ReviewBase):
 
 class ReviewerBase(BaseModel):
     reviewer_name: str
+    skin_type_trouble_prone: bool
     skin_type_oily: bool
-    skin_type_dry: bool
     skin_type_sensitive: bool
+    skin_type_dry: bool
+    skin_type_mildly_dry: bool
     skin_type_combination: bool
-    skin_concern_acne: bool
-    skin_concern_pigmentation: bool
-    skin_concern_blackhead: bool
+    skin_type_normal: bool
+    personal_color_spring_warm: bool
+    personal_color_cool: bool
+    personal_color_autumn_warm: bool
+    personal_color_winter_cool: bool
+    personal_color_summer_cool: bool
+    personal_color_warm: bool
+    skin_concern_keratin: bool
+    skin_concern_pores: bool
+    skin_concern_blackheads: bool
+    skin_concern_excess_sebum: bool
+    skin_concern_whitening: bool
     skin_concern_redness: bool
-    skin_concern_wrinkles_whitening: bool
-    cleansing_power_very_satisfied: bool
-    cleansing_power_average: bool
-    cleansing_power_somewhat_disappointed: bool
-    cleansing_power_very_disappointed: bool
-    irritation_level_no_irritation: bool
-    irritation_level_slight_irritation: bool
-    irritation_level_moderate_irritation: bool
-    irritation_level_severe_irritation: bool
-    absorption_power_very_satisfied: bool
-    absorption_power_average: bool
-    absorption_power_somewhat_disappointed: bool
-    absorption_power_very_disappointed: bool
-    texture_power_very_satisfied: bool
-    texture_power_average: bool
-    texture_power_somewhat_disappointed: bool
-    texture_power_very_disappointed: bool
+    skin_concern_wrinkles: bool
+    skin_concern_trouble: bool
+    skin_concern_dark_circles: bool
+    skin_concern_elasticity: bool
+    skin_concern_atopy: bool
+    skin_concern_spots: bool
+
 
 class ReviewerCreate(ReviewerBase):
     pass
@@ -68,37 +73,27 @@ class ProductBase(BaseModel):
     original_price: int
     final_price: int
     number_of_reviews: int
-    review_rating: float
-    review_5_star_ratio: float
-    review_4_star_ratio: float
-    review_3_star_ratio: float
-    review_2_star_ratio: float
-    review_1_star_ratio: float
+    review_rating: int
+    review_5_star_ratio: int
+    review_4_star_ratio: int
+    review_3_star_ratio: int
+    review_2_star_ratio: int	
+    review_1_star_ratio: int	
     skin_type_dry: int
-    skin_type_sensitive: int
-    skin_type_oily: int
     skin_type_combination: int
-    skin_concern_acne: int
-    skin_concern_pigmentation: int
-    skin_concern_blackhead: int
-    skin_concern_redness: int
+    skin_type_oily: int
+    skin_concern_moisturizing: int
+    skin_concern_soothing: int
     skin_concern_wrinkles_whitening: int
     cleansing_power_very_satisfied: int
     cleansing_power_average: int
     cleansing_power_somewhat_disappointed: int
-    cleansing_power_very_disappointed: int
-    irritation_level_no_irritation: int
-    irritation_level_slight_irritation: int
-    irritation_level_moderate_irritation: int
-    irritation_level_severe_irritation: int
-    absorption_power_very_satisfied: int
-    absorption_power_average: int
-    absorption_power_somewhat_disappointed: int
-    absorption_power_very_disappointed: int
-    texture_power_very_satisfied: int
-    texture_power_average: int
-    texture_power_somewhat_disappointed: int
-    texture_power_very_disappointed: int
+    spreadability_very_satisfied: int
+    spreadability_average: int
+    spreadability_somewhat_disappointed: int
+    irritation_level_not_irritating: int
+    irritation_level_average: int
+    irritation_level_irritating: int
 
 class ProductCreate(ProductBase):
     pass
