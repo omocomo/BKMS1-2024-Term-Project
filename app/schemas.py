9d4bd7c8ade14f711d2485ab49f5290234568713
unknownburphy <chrisandjj1218@gmail.com>
2024-05-29 21:51:17 +0900

    feat: schema optional field update

--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import List, Optional
 from datetime import datetime
 
+
 class ReviewBase(BaseModel):
     product_name: str
     reviewer_name: str
@@ -11,15 +12,16 @@ class ReviewBase(BaseModel):
     skin_type_review: str
     skin_concern_review: str
     irritation_level_review: str
-    cleansing_power_review: Optional[str]
-    spreadability_review: Optional[str]
+    cleansing_power_review: Optional[str] = None
+    spreadability_review: Optional[str] = None
     review_content: str
-    review_date: Optional[datetime]
+    review_date: Optional[datetime] = None
 
 
 class ReviewCreate(ReviewBase):
     product_id: int
     reviewer_id: int
+
 
 class Review(ReviewBase):
     review_id: int
@@ -27,80 +29,85 @@ class Review(ReviewBase):
     class Config:
         orm_mode = True
 
+
 class ReviewerBase(BaseModel):
     reviewer_name: str
-    skin_type_trouble_prone: bool
-    skin_type_oily: bool
-    skin_type_sensitive: bool
-    skin_type_dry: bool
-    skin_type_mildly_dry: bool
-    skin_type_combination: bool
-    skin_type_normal: bool
-    personal_color_spring_warm: bool
-    personal_color_cool: bool
-    personal_color_autumn_warm: bool
-    personal_color_winter_cool: bool
-    personal_color_summer_cool: bool
-    personal_color_warm: bool
-    skin_concern_keratin: bool
-    skin_concern_pores: bool
-    skin_concern_blackheads: bool
-    skin_concern_excess_sebum: bool
-    skin_concern_whitening: bool
-    skin_concern_redness: bool
-    skin_concern_wrinkles: bool
-    skin_concern_trouble: bool
-    skin_concern_dark_circles: bool
-    skin_concern_elasticity: bool
-    skin_concern_atopy: bool
-    skin_concern_spots: bool
+    skin_type_trouble_prone: Optional[bool] = None
+    skin_type_oily: Optional[bool] = None
+    skin_type_sensitive: Optional[bool] = None
+    skin_type_dry: Optional[bool] = None
+    skin_type_mildly_dry: Optional[bool] = None
+    skin_type_combination: Optional[bool] = None
+    skin_type_normal: Optional[bool] = None
+    personal_color_spring_warm: Optional[bool] = None
+    personal_color_cool: Optional[bool] = None
+    personal_color_autumn_warm: Optional[bool] = None
+    personal_color_winter_cool: Optional[bool] = None
+    personal_color_summer_cool: Optional[bool] = None
+    personal_color_warm: Optional[bool] = None
+    skin_concern_keratin: Optional[bool] = None
+    skin_concern_pores: Optional[bool] = None
+    skin_concern_blackheads: Optional[bool] = None
+    skin_concern_excess_sebum: Optional[bool] = None
+    skin_concern_whitening: Optional[bool] = None
+    skin_concern_redness: Optional[bool] = None
+    skin_concern_wrinkles: Optional[bool] = None
+    skin_concern_trouble: Optional[bool] = None
+    skin_concern_dark_circles: Optional[bool] = None
+    skin_concern_elasticity: Optional[bool] = None
+    skin_concern_atopy: Optional[bool] = None
+    skin_concern_spots: Optional[bool] = None
 
 
 class ReviewerCreate(ReviewerBase):
     pass
 
+
 class Reviewer(ReviewerBase):
     reviewer_id: int
-    reviews: List[Review] = []
+    # reviews: List[Review] = []
 
     class Config:
         orm_mode = True
 
+
 class ProductBase(BaseModel):
     product_name: str
-    product_category: str
-    brand_name: str
-    original_price: int
-    final_price: int
-    number_of_reviews: int
-    review_rating: int
-    review_5_star_ratio: int
-    review_4_star_ratio: int
-    review_3_star_ratio: int
-    review_2_star_ratio: int	
-    review_1_star_ratio: int	
-    skin_type_dry: int
-    skin_type_combination: int
-    skin_type_oily: int
-    skin_concern_moisturizing: int
-    skin_concern_soothing: int
-    skin_concern_wrinkles_whitening: int
-    cleansing_power_very_satisfied: int
-    cleansing_power_average: int
-    cleansing_power_somewhat_disappointed: int
-    spreadability_very_satisfied: int
-    spreadability_average: int
-    spreadability_somewhat_disappointed: int
-    irritation_level_not_irritating: int
-    irritation_level_average: int
-    irritation_level_irritating: int
+    product_category: Optional[str] = None
+    brand_name: Optional[str] = None
+    original_price: Optional[int] = None
+    final_price: Optional[int] = None
+    number_of_reviews: Optional[int] = None
+    review_rating: Optional[int] = None
+    review_5_star_ratio: Optional[int] = None
+    review_4_star_ratio: Optional[int] = None
+    review_3_star_ratio: Optional[int] = None
+    review_2_star_ratio: Optional[int] = None
+    review_1_star_ratio: Optional[int] = None
+    skin_type_dry: Optional[int] = None
+    skin_type_combination: Optional[int] = None
+    skin_type_oily: Optional[int] = None
+    skin_concern_moisturizing: Optional[int] = None
+    skin_concern_soothing: Optional[int] = None
+    skin_concern_wrinkles_whitening: Optional[int] = None
+    cleansing_power_very_satisfied: Optional[int] = None
+    cleansing_power_average: Optional[int] = None
+    cleansing_power_somewhat_disappointed: Optional[int] = None
+    spreadability_very_satisfied: Optional[int] = None
+    spreadability_average: Optional[int] = None
+    spreadability_somewhat_disappointed: Optional[int] = None
+    irritation_level_not_irritating: Optional[int] = None
+    irritation_level_average: Optional[int] = None
+    irritation_level_irritating: Optional[int] = None
+
 
 class ProductCreate(ProductBase):
     pass
 
+
 class Product(ProductBase):
     product_id: int
-    reviews: List[Review] = []
+    # reviews: List[Review] = []
 
     class Config:
         orm_mode = True
